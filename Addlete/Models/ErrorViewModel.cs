54972db02e202febdf43a6db7303bff003844fc0
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2024-03-12 01:23:24 +0800

    Add project files.

---
+++ Addlete/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Addlete.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
